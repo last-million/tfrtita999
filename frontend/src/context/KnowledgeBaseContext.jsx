81d354629287cdf0bcfc50b6e765db0dc83edd18
last-million <hamzameliani1@gmail.com>
2025-04-09 21:08:42 +0100

    Update KnowledgeBaseContext.jsx

--- frontend/src/context/KnowledgeBaseContext.jsx
+++ frontend/src/context/KnowledgeBaseContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useReducer, useCallback } from 'react'
-import { api } from '/src/services/api.js' // Try absolute path from src
+import api from '../services/api.js' // Changed to default import
 
 const KnowledgeBaseContext = createContext(null)
 
