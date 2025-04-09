772e13169769b45886cb451c999c8723cf81bb4e
last-million <hamzameliani1@gmail.com>
2025-04-09 22:12:38 +0100

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 // frontend/src/components/Login.jsx
 import React, { useState, useEffect } from "react";
-import { api } from "../services/api";
+import api from "../services/api.js"; // Use default import and add .js
 
 const Login = () => {
   const [username, setUsername] = useState("");
